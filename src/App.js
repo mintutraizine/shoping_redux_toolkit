3a6906d3ced5e99c3031a6581a8be4a856d6e499
Lavanya Lakshita <lavanyal@triazinesoft.com>
2023-03-10 15:51:49 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import { PersistGate } from 'redux-persist/integration/react';
-import store,{persistor} from './store/store';
-import MuiNavbar from './components/MuiNavbar';
-import ProductDetails from './pages/productdetails/ProductDetails';
-import Cart from './pages/cartScreen/Cart';
-import ContactUs from './pages/ContactUs/ContactUs';
-import Footer from './components/Footer';
-const navitmelist=[{ScreenName:'Home',Path:'/'},{ScreenName:'About Us',Path:'/Services'},
-{ScreenName:'Showcase',Path:'/Services'},{ScreenName:'Headphones',Path:'/'},
-{ScreenName:'Merchandise',Path:'/Services'},{ScreenName:'Contact Us',Path:'/contactus'}]
-
-function App() {
-    return (
-        <div className="App">
-        <Provider store={store}>
-            <PersistGate persistor={persistor} loading={null}>
-            <BrowserRouter>
-               <MuiNavbar navitems={navitmelist}/>
-                <Routes>
-                    <Route path="/" element={<Home />}></Route>
-                    <Route path="/cart" element={<Cart />}></Route>
-                    <Route path="/productdetails" element={<ProductDetails />}></Route>
-                    <Route path="/contactus" element={<ContactUs />}></Route>
-                </Routes>
-                <Footer/>
-            </BrowserRouter>
-            </PersistGate>
-        </Provider>
-=======
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -50,6 +14,7 @@ import SignInScreen from "./pages/SignInscreen/SignInScreen";
 
 import SignupScreen from "./pages/SignupScreen/SignupScreen";
 import OtpScreen from "./pages/OtpScreen/OtpScreen";
+import Footer from "./components/Footer";
 const navitmelist = [
   { ScreenName: "Home", Path: "/" },
   { ScreenName: "About Us", Path: "/Services" },
@@ -78,27 +43,10 @@ function App() {
               <Route path="/SignupScreen" element={<SignupScreen />}></Route>
               <Route path="/OtpScreen" element={<OtpScreen />}></Route>
             </Routes>
+            <Footer/>
           </BrowserRouter>
         </PersistGate>
       </Provider>
->>>>>>> 9e69738c75eb44de6bf81085efbae1b8b7abf8b1
-    </div>
-  );
-}
 
-/*  return (
-        <div className="App">
-            <Provider store={store}>
-                <PersistGate persistor={persister} loading={null}>
-                <BrowserRouter>
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Home />}></Route>
-                        <Route path="/cart" element={<Cart />}></Route>
-                    </Routes>
-                </BrowserRouter>
-                </PersistGate>
-            </Provider>
-        </div>
-    ); */
+    </div>)}
 export default App;
--- src/static/imagepaths.js
+++ src/static/imagepaths.js
@@ -1,5 +1,4 @@
 const imagepaths = {
-<<<<<<< HEAD
   newLogo: require('./images/vibe_logo.png'),
   Secure_Checkout: require('./images/Secure_Checkout.png'),
   Youtube_icon: require("./images/Youtube.png"),
@@ -12,15 +11,3 @@ const imagepaths = {
 }
 
 export default imagepaths;
-=======
-  newLogo: require("./images/vibe_logo.png"),
-  Secure_Checkout: require("./images/Secure_Checkout.png"),
-  appleIcon: require("./images/apple.png"),
-  backgroundimage: require("./images/background.png"),
-  facebookIcon: require("./images/facebook.png"),
-  googleIcon: require("./images/Google.png"),
-  otpBackground: require("./images/otpbackground.png"),
-};
-
-export default imagepaths;
->>>>>>> 9e69738c75eb44de6bf81085efbae1b8b7abf8b1
